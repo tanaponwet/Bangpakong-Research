dd57f32204e6ab12eda0691c1d8199faa4e525cf
joepurin2000 <75898167+joepurin2000@users.noreply.github.com>
2023-03-07 02:52:26 +0700

    add graph chart

---
+++ ReactJS/br-react-project/src/components/Graph.js
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+import "../styles/Graph.css"
+import SimpleLineChart from "./SimpleLineChart";
+
+import ShowChartIcon from '@mui/icons-material/ShowChart';
+
+function Graph (props) {
+    // const {data} = props;
+        return (
+            <main className="graph-box">
+                <div className="header">
+                <div className="title-1">
+                    <ShowChartIcon className='icon' sx={{ fontSize: 24 }} />
+                    <p className='text-1'>เปรียบเทียบค่าความเค็ม (g/l) ระหว่าง เครื่องวัด กับ กรมชลประทาน 24 ชม. ที่ผ่านมา</p>
+                </div>
+            </div>
+                <div className="graph-con">
+                    <SimpleLineChart data={props}/>
+                </div>
+            </main>
+        )
+    
+}
+
+export default Graph;
